79b90c5d3c7d6eb39a8152b35133994646fc3e61
natapunch <natapunch7@gmail.com>
2017-03-21 22:41:17 +0000

    Init Framework

---
+++ src/Application.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *Class App
+ */
+
+namespace Punchenko\Framework;
+
+
+class Application
+{
+    /**
+     * Application constructor.
+     */
+    public function run()
+    {
+
+    }
+
+    public function __construct()
+    {
+    }
+
+    public function destract()
+    {
+
+    }
+
+}

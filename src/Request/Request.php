70a80ac0ff607a6e70f4de71edb03a1eed27e1e5
natapunch <natapunch7@gmail.com>
2017-03-27 13:05:51 +0000

    Add Request

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -1,26 +1,39 @@
 <?php
 
-namespace Punchenko\Framework;
+namespace Punchenko\Framework\Request;
 
 
 /**
  * Class Request Singleton
- * @package Punchenko\Framework
+ * @package Punchenko\Framework\Request
  */
 class Request
 {
     private static $request = null;
+    protected $headers = [];
+    protected $raw = '';
+    protected $buffer = '';
 
     /**
      * Request constructor.
      */
     public function __construct()
     {
+        $headers = [];
+        foreach ($_SERVER as $key => $value) {
+            if (substr($key, 0, 5) == "HTTP_") {
+                $key = str_replace(" ", "-", ucwords(strtolower(str_replace("_", " ", substr($key, 5)))));
+                $headers[$key] = $value;
+            } else {
+                $headers[$key] = $value;
+            }
+        }
+        $this->headers = $headers;
     }
+
 
     /**
      * Returns request
-     *
      * @return Request
      */
     public static function getRequest(): self
@@ -37,7 +50,9 @@ class Request
      */
     public function getUri(): string
     {
-        return $_SERVER["REQUEST_URI"];
+        $raw = $_SERVER["REQUEST_URI"];
+        $buffer = explode('?', $raw);
+        return array_shift($buffer);
     }
 
     /**
@@ -53,5 +68,16 @@ class Request
     {
     }
 
-
+    /**
+     * Get request header value
+     * @param null $key
+     * @return mixed
+     */
+    public function getHeader($key = null)
+    {
+        if (empty($key)) {
+            return $this->headers;
+        }
+        return isset($this->headers[$key]) ? $this->headers[$key] : null;
+    }
 }
